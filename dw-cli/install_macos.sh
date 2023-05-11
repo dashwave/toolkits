3f1567497fa078d4e34f565737d321e0b3c22962
Supratik Das <rick.das08@gmail.com>
2023-05-11 23:51:46 +0530

    Add macos installer script

---
+++ dw-cli/install_macos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function install_sshpass() {
+    brew install esolitos/ipa/sshpass
+}
+
+function install_rsync() {
+    brew install rsync
+}
+
+# function install_dwcli() {
+
+# }
+
+install_sshpass
+install_rsync
