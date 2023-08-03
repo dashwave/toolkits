c61134bed210c7e80c76931a5c408081d64e1c20
Supratik Das <rick.das08@gmail.com>
2023-08-04 04:11:33 +0530

    Run chown on a .dw-cli directory

--- install.sh
+++ install.sh
@@ -249,6 +249,9 @@ install_dwcli() {
         fi
 
         sudo -u $RUNNER_USERNAME bash -c '${CONFIG_CMD}'
+
+        chown -R $RUNNER_USERNAME ${HOME}/.dw-cli
+
         sudo -u $RUNNER_USERNAME bash -c '${SUCCESS_CMD}'
     fi
 }
